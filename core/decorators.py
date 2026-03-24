3abc88b28b1ce6edd6fb1f9efa1633e278e52499
sathwikabethu <bethusathwika@gmail.com>
2026-03-25 00:58:48 +0530

    fix(auth): allow django superusers to access custom admin dashboards

--- core/decorators.py
+++ core/decorators.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import user_passes_test
 
 def admin_required(function=None, redirect_field_name='next', login_url='login'):
     actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_admin,
+        lambda u: u.is_active and (u.is_admin or u.is_superuser),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
